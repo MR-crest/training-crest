23ca7f3fd0613cf81e6a03849282a0581c5451a6
mr-crest <meet.r@crestinfosystems.com>
2025-08-22 16:57:37 +0530

    Module 11: Authenticate Requests Complete!!

--- 05-FastAPI/TodoApp/main.py
+++ 05-FastAPI/TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
-from routers import admin, auth, todos  # , users
+from routers import admin, auth, todos, users
 
 app = FastAPI()
 
@@ -11,4 +11,4 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
-# app.include_router(users.router)
+app.include_router(users.router)
